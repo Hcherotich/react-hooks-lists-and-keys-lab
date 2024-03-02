4f4ce3f7ff984ae6655846beb7a27e24a320d24a
cherotich <cherohellen14@gmail.com>
2024-03-02 07:53:31 -0800

    lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+
 
 function App() {
+  const projects = [
+    { id: 1, name:  'Reciplease', technologies:  ['Rails', 'Bootstrap CSS'] },
+    { id: 2, name:  'Kibbles N Bitz', technologies:  ['React', 'Redux'] },
+    { id: 3, name: 'Alienwares', technologies:  ['React', 'Redux', 'Rails'] }
+  ];
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+            <ProjectList projects={projects} />
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -4,8 +4,8 @@ function Links({ github, linkedin }) {
   return (
     <div>
       <h3>Links</h3>
-      <a href={github}>{github}</a>
-      <a href={linkedin}>{linkedin}</a>
+      <a href={'https://github.com/liza'}>{github}</a>
+      <a href={'https://www.linkedin.com/in/liza'}>{linkedin}</a>
     </div>
   );
 }
